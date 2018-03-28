3b813ac9e7bdea4bcb431707dc74fb379aa05d48
Tim <tjhong30@github.com>
2018-03-27 17:12:20 -0700

    checkpoint

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './styles.css'
+
+
+class App extends Component {
+   render() {
+      return (
+         <div>
+            Hello World
+         </div>
+      );
+   }
+}
+export default App;
