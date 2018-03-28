39868faa3e3f87fbe0b10d08298377e01f0647f0
johnhpark <30715332+johnhpark@users.noreply.github.com>
2018-03-27 20:17:32 -0700

    Added controllers for user login based on google OAuth and modularized express server

--- app/models/database.js
+++ app/models/database.js
@@ -1,14 +1,11 @@
 const pg = require('pg'); // PostgreSQL
-
-const db = {};
-const config = require('./../server/config');
+const config = require('./../secrets/config');
 const uri = process.env.ELEPHANTSQL_URL || config.url;
 
 // initialization of sql server
 const client = new pg.Client(uri);
 client.connect((err, db_) => {
     if (err) return console.error('could not connect to postgres', err);
-    db.conn = db_;
 });
 
-module.exports = db;
+module.exports = client;
--- app/server/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'postgres://nmvtdfbj:7umwKkAL4ho08DjGI6FtoRxUFyK26f0E@stampy.db.elephantsql.com:5432/nmvtdfbj',
-};
