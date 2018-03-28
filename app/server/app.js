1fecba28c066278192e51a0f101cf37d36da38e4
Tim <tjhong30@github.com>
2018-03-28 14:18:42 -0700

    addedConsole

--- app/server/app.js
+++ app/server/app.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.static("public"));
-
+console.log('hello');
 // Users
 app.post(
   "/loginUser",
