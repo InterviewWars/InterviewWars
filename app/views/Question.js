56f1582e01d8c4d1d27cee3312bbf3788a187cbe
scottyb <scottyb@scottybs-MacBook.local>
2018-03-29 16:28:25 -0700

    updated questions page categories

--- app/views/Question.js
+++ app/views/Question.js
@@ -1,38 +1,50 @@
-import React, { Component } from "react";
-import Button from "material-ui/Button";
-import { render } from "react-dom";
-import { Link } from "react-router-dom";
-import TextField from "material-ui/TextField";
+import React, { Component } from 'react';
+import Button from 'material-ui/Button';
+import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+import TextField from 'material-ui/TextField';
 
 const style = {
-  margin: 12,
-  backgroundColor: "#0b0c0c",
-  color: "black"
-};
+    margin: 12,
+    backgroundColor: 'black',
+    color: 'black'
+  }; 
 
-class Question extends Component {
-  constructor(props) {
+  class Question extends Component {
+    constructor(props) {
     super(props);
-    this.state = {};
-    // binding here
-  }
+    this.state = {
 
-  render() {
+    };
+    // binding here
+}
+
+render() {
     return (
-      <div>
+        <div id="parent2">
         <div>
-          <h1> Thanks for adding your interview question! </h1>
-          <h2> Enter here</h2> <text-area rows="10" cols="60" />
+            <h1> Thanks for adding your interview question! </h1>
+            <h2> <textarea
+  rows="10"
+  cols="60"
+/> 
+</h2>
+</div>
+<div>
+<TextField > </TextField > Difficulty
+</div>
+<div>
+<TextField > </TextField > Type
+</div>
+<div>
+<TextField > </TextField > Company
+</div>
+
+ <Button label="Submit" style={style} > Submit
+ </Button >
         </div>
-        <div>
-                <Button label="Submit" style={style}>
-                    {" "}
-                    Submit{" "}
-                </Button>
-        </div>
-      </div>
-    );
-  }
+    )
+}
 }
 
 export default Question;
