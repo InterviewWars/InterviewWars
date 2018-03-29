327171ed0d152ecc865e0c3723afdebbd1928578
scottyb <scottyb@scottybs-MacBook.local>
2018-03-28 17:29:15 -0700

    submit questions button

--- Home.js
+++ Home.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import Button from 'material-ui/Button';
 import { render } from 'react-dom';
+import { Link } from 'react-router-dom';
+
+const style = {
+    margin: 12,
+    backgroundColor: 'teal',
+    color: 'black'
+  }; 
 
 class Home extends Component {
     constructor(props) {
@@ -13,8 +20,8 @@ class Home extends Component {
 render() {
     return (
         <div>
-            <h1> Welcome Interview Wars! </h1>
-            <h2> Please help other users by answering a question or submitting an answer </h2>
+            <h1> Welcome to Interview Wars! </h1>
+            <h2> Please answer a question below or submit a new question  <Button label="SubmitQ" style={style} > <Link to="/">Submit New </Link> </Button ></h2>
         </div>
     )
 }
